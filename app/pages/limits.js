b896fbb68c98741b56de4a47cde76d880f917cd2
Y2X <1002138632@qq.com>
2016-11-21 00:31:30 +0800

    修改limits的ui

--- app/pages/limits.js
+++ app/pages/limits.js
@@ -18,7 +18,7 @@ export default class extends BasePage {
     apiSuccess(url, body) {
         this.showLoading(false);
         switch (url) {
-            case UrlConfig.url+"?url=http://b.fencaike.com/api/"+UrlConfig.competition:
+            case UrlConfig.url + "?url=http://b.fencaike.com/api/" + UrlConfig.competition:
                 var ll = JSON.parse(body);
                 this.setState({
                     list: ll.data || []
@@ -31,26 +31,87 @@ export default class extends BasePage {
         super.componentDidMount();
 
         this.showLoading(true)
-        ApiAction.post(UrlConfig.url+"?url=http://b.fencaike.com/api/"+UrlConfig.competition);
+        ApiAction.post(UrlConfig.url + "?url=http://b.fencaike.com/api/" + UrlConfig.competition);
 
     }
 
-    renderItem(item){
-        return item.map(function(item, index){
-            return(
-                <div className="item" key={'item'+index}>
+
+    renderItemNew(item) {
+        var type;
+        return item.map(function (item, index) {
+            switch (item.limit_type) {
+                case '连续不胜':
+                    type = 'type-1';//橙色
+                    break;
+                case '连续不平':
+                    type = 'type2';//绿色
+                    break;
+                case '连续不负':
+                    type = 'type3';//紫色
+                    break;
+                case '连续赢盘':
+                    type = 'type4';//蓝色
+                    break;
+                default :
+                    type = 'type1';
+            }
+            return (
+                <div className="item" key={'item' + index}>
+                    <div className={"limitType " + type}>
+                        {item.limit_type}
+                    </div>
+                    <div className="content">
+                        <div className="topInfo">
+                            <div className="leftInfo">
+                                <span className="title">{item.third_level}</span>
+                                <span className="time">{item.code}</span>
+                                <div className="teamName">{item.jc_team_name}</div>
+                            </div>
+                            <div className="rightInfo">
+                                <div className="limitNum"><span>{item.limit_num}</span>场</div>
+                                <div>临场极限</div>
+                            </div>
+                        </div>
+                        <div className="bottomInfo">
+                            <div className="limitItem">
+                                <span>历史极限</span>
+                                <span>近1年</span>
+                                <span>{item.one_his_num}</span>
+                                <span>近10年</span>
+                                <span>{item.ten_his_num}</span>
+                            </div>
+                            <div className="limitSurpass">
+                                <span>超出历史极限</span>
+                                <span>近1年</span>
+                                <span>{item.one_his_end_time.substr(2, 2) + '/' + item.one_his_end_time.substr(5, 2) + '/' + item.one_his_end_time.substr(8, 2)}</span>
+                                <span>近10年</span>
+                                <span>{item.ten_his_end_time.substr(2, 2) + '/' + item.ten_his_end_time.substr(5, 2) + '/' + item.ten_his_end_time.substr(8, 2)}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )
+        })
+    }
+
+    renderItem(item) {
+        return item.map(function (item, index) {
+            return (
+                <div className="item" key={'item' + index}>
                     <div className="title">
                         {item.limit_type}
                     </div>
                     <div className="lline">
                         <span className="l1">{item.code}</span>
-                        <span className="l2" style={{backgroundColor:item.backgroundcolor}}>{item.third_level}</span>
+                        <span className="l2" style={{backgroundColor: item.backgroundcolor}}>{item.third_level}</span>
                         <span className="l3">{item.jc_team_name}</span>
                         <span className="l4">{item.limit_num + "场"}</span>
                         <span className="l5">{item.one_his_num}</span>
                         <span className="l6">{item.ten_his_num}</span>
-                        <span className="l7">{item.one_his_end_time.substr(2,2) + '/' + item.one_his_end_time.substr(5,2) + '/' + item.one_his_end_time.substr(8,2)}</span>
-                        <span className="l8">{item.ten_his_end_time.substr(2,2) + '/' + item.ten_his_end_time.substr(5,2) + '/' + item.ten_his_end_time.substr(8,2)}</span>
+                        <span
+                            className="l7">{item.one_his_end_time.substr(2, 2) + '/' + item.one_his_end_time.substr(5, 2) + '/' + item.one_his_end_time.substr(8, 2)}</span>
+                        <span
+                            className="l8">{item.ten_his_end_time.substr(2, 2) + '/' + item.ten_his_end_time.substr(5, 2) + '/' + item.ten_his_end_time.substr(8, 2)}</span>
                     </div>
                 </div>
             )
@@ -60,8 +121,8 @@ export default class extends BasePage {
     renderList() {
         return this.state.list.map(function (item, index) {
             return (
-                <div className="listS" key={'list'+index}>
-                    {this.renderItem(item)}
+                <div className="listS" key={'list' + index}>
+                    {this.renderItemNew(item)}
                 </div>
             )
         }.bind(this));
@@ -73,16 +134,6 @@ export default class extends BasePage {
         return (
             <Layout hideBack={false} className={'limits'} title={'极限追盘'}>
                 <Loading showLoading={this.state.showLoading}/>
-                <div className="limits-header">
-                    <span className="l1">{"编号"}</span>
-                    <span className="l2">{"赛事类型"}</span>
-                    <span className="l3">{"球队名称"}</span>
-                    <span className="l4">{"临场极限"}</span>
-                    <span className="l5">{"近1年"}</span>
-                    <span className="l6">{"近10年"}</span>
-                    <span className="l7">{"超出时间"}</span>
-                    <span className="l8">{"超出时间"}</span>
-                </div>
                 {this.renderList()}
             </Layout>
         )
